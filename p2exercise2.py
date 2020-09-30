aa22de624f0e13855dea9d033c2767ba04bba51d
Anas Nadeem <anas5678go@gmail.com>
2020-10-01 00:20:00 +0530

    Updated

--- p2exercise2.py
+++ p2exercise2.py
@@ -1,18 +1,18 @@
-print("this is a greate calculator for your calculation my bro")
+print("this is a faulty calculator my bro")
 a=float(input("give your no."))
 b=float(input("your 2nd no."))
-c=input("what you want sum=+,SUBS=-,div=/,mult=*")
-if a==45 and b==3 and c=="*":
+c=input("what you want? Press 1 for sum 2 for minus 3 for div and 4 for mul")
+if a==45 and b==3 and c=="4":
     print(555)
-elif a==56 and b==9 and c=="+":
+elif a==56 and b==9 and c=="1":
     print(77)
-elif c=="+":
+elif c=="1":
     print(a+b)
-elif c=="-":
+elif c=="2":
     print(a-b)
-elif c=="/":
+elif c=="3":
     print(a/b)
-elif c=="*":
+elif c=="4":
     print(a*b)
 else:
-    print("TANKS FOR USING US")
+    print("Please input a correct number")
