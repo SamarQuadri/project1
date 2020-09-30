60abe77b6a941c8303041d1f31479dfdc74b5f4d
Samar Quadri <quadrisamar@gmail.com>
2020-09-30 23:49:33 +0530

    this is a calculator

---
+++ p2exercise2.py
@@ -0,0 +1,18 @@
+print("this is a greate calculator for your calculation my bro")
+a=float(input("give your no."))
+b=float(input("your 2nd no."))
+c=input("what you want sum=+,SUBS=-,div=/,mult=*")
+if a==45 and b==3 and c=="*":
+    print(555)
+elif a==56 and b==9 and c=="+":
+    print(77)
+elif c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="/":
+    print(a/b)
+elif c=="*":
+    print(a*b)
+else:
+    print("TANKS FOR USING US")
